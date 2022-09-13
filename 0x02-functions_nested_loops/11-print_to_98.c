77ffc2f57323ec102c2991aa1d9f569605830069
jainam <jainamsharma7@gmail.com>
2022-09-13 13:09:09 -0700

    changes

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,19 +8,40 @@
 */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n < 98)
 	{
-		if (n != 98)
+		while (n <= 98)
 		{
-			printf("%d", n);
-			putchar(',');
-			putchar(' ');
-		}
-		else
-		{
-			printf("%d", n);
+			if (n != 98)
+			{
+				printf("%d", n);
+				putchar(',');
+				putchar(' ');
+			}
+			else
+			{
+				printf("%d", n);
+			}
+			n++;
 		}
 		putchar('\n');
-		n++;
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			if (n != 98)
+			{
+				printf("%d", n);
+				putchar(',');
+				putchar(' ');
+			}
+			else
+			{
+				printf("%d", n);
+			}
+			n--;
+		}
+		putchar('\n');
 	}
 }
