993bf54776eebc30c60a93b463485bbf619182cb
jainam <jainamsharma7@gmail.com>
2022-09-15 05:35:34 -0700

    multiplies two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *mul - multiplies two numbers
+ *@a: input number 1
+ *@b: input number 2
+ *Return: the product of the two inputs
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -27,3 +27,5 @@ void print_times_table(int n);
 int _isupper(int c);
 
 int _isdigit(int c);
+
+int mul(int a, int b);
