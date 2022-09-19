0cde13b402947a4dbafe95202f02db5f5c7255f8
jainam <jainamsharma7@gmail.com>
2022-09-19 04:42:51 -0700

    function to use pointers to reset value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *reset_to_98 - function to change value using pointers
+ *@n: input pointer
+ *Return: nothing
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Projects on pointers
+0-reset_to_98.c - uses pointers to re-assign values
