0fbf0320d1d14d8ecacc5fb651d03fa656d418b1
jainam <jainamsharma7@gmail.com>
2022-09-19 09:34:54 -0700

    prints every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *puts2 - return every other character
+ *@str: input str
+ *Return: nothing
+*/
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
