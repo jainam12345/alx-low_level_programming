8ffb39440c10821973741b580e487511d7eb78f0
jainam <jainamsharma7@gmail.com>
2022-09-26 00:45:43 -0700

    project 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ **_memset - fills n byes of a string
+ *@s: target mem area
+ *@n: number of bytes to replace
+ *@b: char to replace with
+ *Return: the address of s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n && *s != '\0')
+	{
+		*s = b;
+		s++;
+		i++;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Pointers and arrays
+0-memset.c - replaces a set no. of bytes to a spepcified string
