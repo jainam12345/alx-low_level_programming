ff68c00ab0ead5741de4b3053a15fc7f7facfc9f
jainam <jainamsharma7@gmail.com>
2022-09-26 06:10:46 -0700

    changes

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_strspn - return length of first substring
+ *@s: searching string
+ *@accept: check string
+ *Return: length of first substring
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int counter = 0;
+	int i,j,k;
+
+	for (k = 0; s[k] != ' ' && s[k] != ','; k++)
+	{
+		continue;
+	}
+
+	for (i = 0; i < k; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				counter++;
+			}
+		}
+	}
+
+	return (counter);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@ Pointers and arrays
 1-memcpy.c - copies n bytes from mem src to dest
 
 2-strchr.c - return location to a character
+3-strspn.c - return the length of first sub-string that contains the characters in ref str
