dc2f6e91464b0e00a1328416fbd78996249911b4
jainam <jainamsharma7@gmail.com>
2022-09-30 02:04:24 -0700

    changes

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+/**
+ *main - prints program name only
+ *@argc: number of arguments passed at command line
+ *@argv: pointers to the argument strings
+ *Return: 0 if success and  otherwise
+*/
 int main(int argc, char *argv[])
 {
 	if (argc == 1)
