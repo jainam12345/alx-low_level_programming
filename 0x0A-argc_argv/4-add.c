161aeac782a4a083eab272a4c776747e54860adc
jainam <jainamsharma7@gmail.com>
2022-09-30 05:42:09 -0700

    adds numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - return sum of numbers
+ *@argc: number of arguments
+ *@argv: pointers to arguments
+ *Return: 0 if success and 1 if fail
+*/
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
+			{
+				j++;
+			}
+			else
+			{
+				printf("Error");
+				putchar('\n');
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("The sum is %d\n", sum);
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -3,3 +3,4 @@ argc and arg v projects
 1-args.c - prints number of arguments passed
 2-args.c - prints all arguments receieved
 3-mul.c - multiplies 2 numbers from args
+4-add.c - adds positive numbers
