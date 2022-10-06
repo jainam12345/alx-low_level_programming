4bcf8e2907495af9b077e8b3a7ae2a1f379b4f60
jainam <jainamsharma7@gmail.com>
2022-10-06 02:36:19 -0700

    changes

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ **string_nconcat - concatenates two strings
+ *@s1: string 1
+ *@s2: string 2
+ *@n: number of bytes to concatenate
+ *Return: pointer to concatenated string memory
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0, k, counter = 0, j = 0, total_char;
+	char *new;
+
+	k = 0;
+	while (*(s1 + k) != '\0')
+	{
+		counter++;
+		k++;
+	}
+	total_char = counter + n + 1;
+	new = malloc(total_char);
+	if (new == NULL)
+		return (NULL);
+	while (*s1 != '\0')
+	{
+		new[i] = *s1;
+		s1++;
+		i++;
+	}
+	while (j < n && s2[j] != '\0')
+	{
+		new[i] = s2[j];
+		i++;
+		j++;
+	}
+
+	return (new);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,4 @@
 More malloc and free projects
 
 0-malloc_checked.c - allocates memory and return pointer address
+1-string_nconcat.c - string concat to n bytes
