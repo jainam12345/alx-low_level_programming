d417ec1fd418db074067e7513ade6bc839d59e12
jainam <jainamsharma7@gmail.com>
2022-10-10 01:10:59 -0700

    chanegs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
-struct dog {
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+*struct dog - defines a structure for a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner of the dog
+*Description: defines the properties of a dog
+*/
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
-	
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
